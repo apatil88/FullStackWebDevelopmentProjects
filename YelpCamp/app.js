29631c0f6a51d2d07441b94e0183617523dae865
apatil88 <amrut.patil88@hotmail.com>
2017-03-12 19:50:54 -0400

    Added partials for header and footer. Integrated bootstrap. Added new POST route to create a new playground.

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -1,20 +1,43 @@
 var express = require("express");
 var app = express();
 
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended : true}));
+
 app.set("view engine", "ejs");
+
+//TODO : Move to MongoDB database
+var campgrounds = [
+       {name : "Beach camping", image: "https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg"},
+       {name : "Wye Valley Camping", image: "https://farm8.staticflickr.com/7042/7121867321_65b5f46ef1.jpg"},
+       {name : "Camp", image: "https://farm2.staticflickr.com/1363/1342367857_2fd12531e7.jpg"}
+       
+       ]; 
 
 app.get("/",  function(req, res){
     res.render("landing");
 });
 
 app.get("/campgrounds", function(req, res){
-   var campgrounds = [
-       {name : "Beach camping", image: "https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg"},
-       {name : "Wye Valley Camping", image: "https://farm8.staticflickr.com/7042/7121867321_65b5f46ef1.jpg"},
-       {name : "Camp", image: "https://farm2.staticflickr.com/1363/1342367857_2fd12531e7.jpg"}
-       
-       ]; 
        res.render("campgrounds", {campgrounds : campgrounds});
+});
+
+//Extract name and image from the POST request sent from the form
+app.post("/campgrounds", function(req, res){
+    //get data from form and add to campgrounds array
+    var name = req.body.name;
+    var image = req.body.image;
+    var newCampground = {name : name, image : image};
+    campgrounds.push(newCampground);
+    
+    //redirect to campgrounds page
+    res.redirect("/campgrounds");  //Default redirect : GET
+});
+
+//Renders a form to create a new campground
+app.get("/campgrounds/new", function(req, res){
+   res.render("new"); 
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
