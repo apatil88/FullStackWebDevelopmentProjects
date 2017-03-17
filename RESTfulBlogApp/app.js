ba589f006f1b0411df398eb1fe2d170a82e5126b
apatil88 <amrut.patil88@hotmail.com>
2017-03-16 22:31:06 -0400

    Added Express Sanitizer to sanitize data submitted via CREATE and UPDATE route. Completed styling on blogs index page.

--- RESTfulBlogApp/app.js
+++ RESTfulBlogApp/app.js
@@ -1,7 +1,9 @@
 var express  = require("express"),
     mongoose = require("mongoose"),
-    bodyParser = require("body-parser");
-    moment   = require("moment");
+    bodyParser = require("body-parser"),
+    moment   = require("moment"),
+    methodOverride = require("method-override"),
+    expressSanitizer = require("express-sanitizer");
     
 var app = express();
 
@@ -10,6 +12,8 @@ mongoose.connect("mongodb://localhost/restful_blog_app");
 app.set("view engine", "ejs");
 app.use(express.static("public"));  //To serve custom CSS
 app.use(bodyParser.urlencoded({extended : true}));  //For parsing the HTTP request body when a form is submitted
+app.use(methodOverride("_method"));   //To handle PUT requests. HTML form do not support PUT and DESTROY methods. They only support GET and POST methods.
+app.use(expressSanitizer());
 
 //MONGOOSE/MODEL CONFIG
 var blogSchema = new mongoose.Schema({
@@ -50,6 +54,8 @@ app.get("/blogs/new", function(req, res){
 
 //CREATE Route : POST
 app.post("/blogs", function(req, res){
+    //Sanitize the request body
+    req.body.blog.body = req.sanitize(req.body.blog.body);
    Blog.create(req.body.blog, function(err, newBlog){
         if(err){
             res.render("new");
@@ -66,6 +72,43 @@ app.get("/blogs/:id", function(req, res){
            res.redirect("/blogs");
        } else {
            res.render("show", {blog: foundBlog});
+       }
+    });
+});
+
+//EDIT ROUTE : GET
+app.get("/blogs/:id/edit", function(req, res){
+    //Find the id of the blog to be added
+    Blog.findById(req.params.id, function(err, foundBlog){
+       if(err) {
+           res.redirect("/");
+       } else {
+           res.render("edit", {blog : foundBlog});
+       }
+    });
+        
+});
+
+//UPDATE ROUTE : PUT
+app.put("/blogs/:id", function(req, res){
+   req.body.blog.body = req.sanitize(req.body.blog.body);
+   Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
+      if(err){
+          res.redirect("/blogs");
+      } else {
+          res.redirect("/blogs/" + req.params.id);
+      }
+   });
+});
+
+//DELETE ROUTE : DELETE
+app.delete("/blogs/:id", function(req, res){
+    //Delete blog and redirect
+    Blog.findByIdAndRemove(req.params.id, function(err){
+       if(err){
+           res.redirect("/blogs");
+       } else {
+           res.redirect("/blogs");
        }
     });
 });
