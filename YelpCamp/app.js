93883bdb4822e681610dd645189eb02b9d17e3c2
apatil88 <amrut.patil88@hotmail.com>
2017-03-21 22:38:23 -0400

    Refactored app.js to move Campgrounds and Comments Schema into a separate models directory

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -3,6 +3,7 @@ var express     = require("express"),
     bodyParser  = require("body-parser"),
     mongoose    = require("mongoose"),
     Campground  = require("./models/campground.js"),
+    Comment     = require("./models/comment.js"),
     seedDB      = require("./seeds");
     
     
@@ -89,6 +90,33 @@ app.get("/campgrounds/:id/comments/new", function(req, res){
        }
     });
   
+});
+
+app.post("/campgrounds/:id/comments", function(req, res){
+   
+   //Find campground by ID
+   Campground.findById(req.params.id, function(err, campground){
+      if(err){
+          console.log(err);
+          res.redirect("/campgrounds");
+      } else {
+          //Create comment
+          Comment.create(req.body.comment, function(err, comment){
+              if(err){
+                  console.log(err);
+              } else {
+                  //Associate comment with campground
+                  campground.comments.push(comment);
+                  campground.save();
+                  
+                  //redirect to campground show page
+                  res.redirect("/campgrounds/" + campground._id);
+              }
+          });
+      }
+   });
+   
+   
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
