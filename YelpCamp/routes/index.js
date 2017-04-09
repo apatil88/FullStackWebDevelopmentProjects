989add1dee441b5a5f7a804e9ce0da874e978674
Amrut Patil <amrut.patil88@gmail.com>
2017-04-09 13:25:16 -0400

    Update index.js

--- YelpCamp/routes/index.js
+++ YelpCamp/routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var passport = require("passport");
 var User = require("../models/user.js");
 
+//Root Route
 router.get("/",  function(req, res){
     res.render("landing");
 });
