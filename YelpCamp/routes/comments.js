92c312b33aa46803487fcdc3d9d4746488a2bd29
apatil88 <amrut.patil88@hotmail.com>
2017-04-09 20:16:55 -0400

    Added functionality to edit comments on a campground

--- YelpCamp/routes/comments.js
+++ YelpCamp/routes/comments.js
@@ -55,6 +55,31 @@ router.post("/campgrounds/:id/comments", isLoggedIn, function(req, res){
    
 });
 
+//COMMENT EDIT ROUTE
+router.get("/campgrounds/:id/comments/:comment_id/edit", function(req, res){
+    Comment.findById(req.params.comment_id, function(err, foundComment){
+        if(err){
+            console.log(err);
+            res.redirect("back");
+        } else {
+           res.render("comments/edit", {campground_id: req.params.id, comment: foundComment})  //Id here refers to campground id 
+        }
+    })
+   
+});
+
+//COMMENT UPDATE ROUTE
+router.put("/campgrounds/:id/comments/:comment_id", function(req, res){
+   Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment){
+     if(err){
+         console.log(err);
+         res.redirect("back");
+     }  else {
+         res.redirect("/campgrounds/"+req.params.id);
+     }
+   });
+});
+
 //Middleware to check if the user is logged in
 function isLoggedIn(req, res, next){
     if(req.isAuthenticated()){
